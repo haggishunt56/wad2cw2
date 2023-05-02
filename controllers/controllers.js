28951e78aa35a3115fbdf120e9c8c0593873f967
haggishunt56 <whatevermaan@hotmail.com>
2023-05-02 23:01:53 +0100

    more user-friendly error messages for login and register. also confirmation of user creation added

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -30,24 +30,49 @@ exports.register_new_user = (req, res) => {
     const password = req.body.password;
     const passwordConf = req.body["password-confirm"];
 
-    if (!username || !password) {
-        res.status(401).send('Please provide all required fields'); // todo - render webpage
-        return;
-    }
-    if (password !== passwordConf) {
-        res.status(401).render('register', {
-            'title': 'Register account'
-        }); // todo - display error and auto-populate with provided details to prevent user having to resupply these
+    // if any field is not supplied, display page again with an error.
+    // username and email fields pre-populated. email address must be re-entered.
+    if (!username || !email || !password || !passwordConf) {
+        res.status(401).render("register", {
+            title: 'Register account',
+            err: "Please provide all required fields.",
+            username: username,
+            email: email
+        });
         return;
     }
 
+    // if passwords do not match, display page again with an error
+    if (password !== passwordConf) {
+        res.status(401).render("register", {
+            title: 'Register account',
+            err: "Passwords do not match. Please re-enter.",
+            username: username,
+            email: email
+        });
+        return;
+    }
+
+    // todo - password validation (8 chars min, 1 LC letter, 1 UC letter & 1 number)
+    // todo email address validation
+
     userDao.lookup(username, function(err, u) {
+        // if user exists, display error
         if (u) {
-            res.status(400).send("Error: User " + username + " already exists!"); // todo - render webpage
+            res.status(401).render("register", {
+                title: 'Register account',
+                err: "User '" + username + "' already exists. Please <a href=\"/login\">log in</a> instead.",
+                username: username,
+                email: email
+            });
             return;
         }
+
+        // once all validation passed, create user and display confirmation on login page
         userDao.create(username, email, password);
-        res.status(200).redirect('/login'); //todo - display message on screen that the account has been created
+        res.status(200).render("login", {
+            confirmation: "User '" + username + "' created. Please log in using the supplied username and password:"
+        });
     });
 }
 
@@ -63,13 +88,18 @@ exports.log_in_user = (req, res) => {
             user: req.body.username
         });
     } else {
-        res.render("login"); // authentication failed
-        //todo - display error on login page
+        // authentication failed
+        res.status(401).render("login", {
+            err: "Incorrect username or password. Please try again."
+        });
     }
 }
 
 exports.logout = (req, res) => {
-    res.clearCookie("jwt").status(200).redirect("/"); // todo - display message that user is logged out.
+    res.clearCookie("jwt").status(200).render("landing", {
+        title: "Home",
+        confirmation: "Successfully logged out."
+    });
 }
 
 exports.home = (req, res) => {
