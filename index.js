85c953fc570fac6e8e48af974a22f82e71e80e03
haggishunt56 <whatevermaan@hotmail.com>
2023-05-05 15:40:08 +0100

    add favicon to all pages

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.set('view engine', 'mustache');
 const applicationRouter = require('./routes/cwRoutes');
 app.use('/', applicationRouter); 
 
-// start server
+// create favicon
+app.use('/favicon.ico', express.static('images/favicon.ico'));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000. Ctrl^c to quit.');
